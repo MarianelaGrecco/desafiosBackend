9eb4ccc008751b44a49fcdcf1118c9a5dfe27c3b
Marianela Grecco <114708413+MarianelaGrecco@users.noreply.github.com>
2023-05-09 21:04:19 -0300

    Nueva corrección

--- Motores de plantillas + Websocket/src/Public/js/main.js
+++ Motores de plantillas + Websocket/src/Public/js/main.js
@@ -1,15 +1,16 @@
+const socket = io();
 
-const socket = io()
+const formProduct = document.getElementById("formProducto");
 
-const formProduct = document.getElementById("formProducto") 
+formProduct.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const prodsIteretor = new FormData(e.target);
+  const prod = Object.fromEntries(prodsIteretor);
+  socket.emit("nuevoProducto", { prod });
+  // Limpiar el formulario
+  formProduct.reset();
+});
 
-formProduct.addEventListener('submit', (e) => {
-    e.preventDefault()
-    const prodsIteretor = new FormData(e.target)
-    const prod = Object.fromEntries(prodsIteretor)
-console.log (prod)
-socket.emit("nuevoProducto", {prod})
-})
 
 
 
