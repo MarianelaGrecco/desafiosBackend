c86fff0148c80334663f0bb6dfdf405f72c999e6
Marianela Grecco <114708413+MarianelaGrecco@users.noreply.github.com>
2023-05-22 20:49:16 -0300

    Practica Integradora

---
+++ primeraParcticaIntegradora/src/routes/product.routes.js
@@ -0,0 +1,111 @@
+import { Router } from "express";
+import { ProductManager } from "../ProductManager.js";
+
+const productManager = new ProductManager("./productos.txt");
+
+const productRouter = Router();
+
+
+productRouter.post("/nuevoProducto", async (req, res) => {
+  const { title, description, price, thumbnail, status, code, stock } = req.body;
+  await productManager.addProduct({
+    title,
+    description,
+    price,
+    thumbnail,
+    status,
+    code,
+    stock,
+  });
+  res.redirect('/');
+});
+
+
+productRouter.get ("/realtimeproducts", async (req, res) => {
+  try{
+    const products = await productManager.getProducts()
+    res.render('realtimeproducts', {products})
+  }catch (error) {
+    res.send(error)
+  }
+})
+
+productRouter.get ("/home", async (req, res) => {
+  try{
+    const products = await productManager.getProducts()
+    res.render('home', {products})
+  }catch (error) {
+    res.send(error)
+  }
+})
+
+productRouter.get ("/chat", async (req, res) => {
+  try{
+    const products = await productManager.getProducts()
+    res.render('chat', {products})
+  }catch (error) {
+    res.send(error)
+  }
+})
+
+productRouter.get("/", async (req, res) => {
+  const products = await productManager.getProducts();
+  const limit = req.query.limit;
+  let result = products;
+
+  if (limit) {
+    const limitNumber = parseInt(limit);
+    result = products.slice(0, limitNumber);
+  }
+  if (result.length > 0) {
+    res.send(JSON.stringify(result));
+  } else {
+    res.status(404).send("No products found");
+  }
+});
+
+productRouter.get("/:pid", async (req, res) => {
+  const products = await productManager.getProductById(req.params.pid);
+  res.json(products);
+});
+
+productRouter.post("/", async (req, res) => {
+  const { title, description, price, thumbnail, status, code, stock } =
+    req.body;
+  await productManager.addProduct({
+    title,
+    description,
+    price,
+    thumbnail,
+    status,
+    code,
+    stock,
+  });
+  res.send("Producto creado");
+});
+
+productRouter.put("/:pid", async (req, res) => {
+  const id = req.params.pid;
+  const { title, description, price, thumbnail, status, code, stock } =
+    req.body;
+
+  const mensaje = await productManager.updateProduct(id, {
+    title,
+    description,
+    price,
+    thumbnail,
+    status,
+    code,
+    stock,
+  });
+
+  res.send(mensaje);
+});
+
+productRouter.delete("/:pid", async (req, res) => {
+  const id = req.params.pid;
+  const mensaje = await productManager.deleteProduct(id);
+  res.send(mensaje);
+});
+
+export default productRouter;
