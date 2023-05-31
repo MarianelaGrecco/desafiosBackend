23051479b8c45d2a00e82906d21f08a4ad276d82
Marianela Grecco <114708413+MarianelaGrecco@users.noreply.github.com>
2023-05-31 15:30:38 -0300

    Proyecto Final

--- segundaPreEntrega/src/models/Cart.js
+++ segundaPreEntrega/src/models/Cart.js
@@ -1,1 +1,17 @@
+import { Schema, model } from "mongoose";
 
+const cartSchema = new Schema({
+  products: {
+    type: [
+      {
+        id_prod: {
+          type: Schema.Types.ObjectID,
+          ref: "products",
+        },
+        cant: Number,
+      },
+    ],
+    default: []
+  },
+});
+export default cartModel = model(cartSchema, "carts")
