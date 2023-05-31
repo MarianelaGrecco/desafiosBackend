0570626f2898682e3032109edaac1e5546e88fd4
Marianela Grecco <114708413+MarianelaGrecco@users.noreply.github.com>
2023-05-31 14:37:07 -0300

    ProyectoFinal

---
+++ segundaPreEntrega/src/models/Products.js
@@ -0,0 +1,37 @@
+import { Schema, model } from "mongoose";
+
+const productSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  code: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  stock: {
+    type: Number,
+    required: true,
+  },
+  status: {
+    type: Boolean,
+    default: true,
+  },
+  thumbnail: [],
+});
+
+// export default productModel = model(productSchema, "products");
+const productModel = model(productSchema, "products");
+export default productModel;
