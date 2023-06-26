f116cbefe7d581d7a4f271c39230741cf48347ff
Marianela Grecco <114708413+MarianelaGrecco@users.noreply.github.com>
2023-06-26 12:40:10 -0300

    desafío7Backend

---
+++ Estrategias de autenticación/src/routes/users.routes.js
@@ -0,0 +1,66 @@
+
+import { Router } from 'express'
+import { usersModel } from '../models/Users.js'
+import { hashData, compareData } from '../path.js'
+import passport from 'passport'
+
+const userRouter = Router()
+
+// persistencia mongo
+
+userRouter.post('/signup', async(req, res) => {
+    const { email, password } = req.body
+    const user = await usersModel.findOne({email})
+    if(user){
+      return res.redirect('/api/views/errorSignup')
+    }
+    const hashPassword = await hashData(password)
+    const newUser = {...req.body,password:hashPassword}
+    await usersModel.create(newUser)
+    res.redirect('/api/views/signup')
+  })
+  
+//login sin passport 
+  // userRouter.post('/login',async (req,res)=>{
+  //   const {email, password} = req.body
+  //   const user = await usersModel.findOne({email})
+  //   if(!user){
+  //     return res.redirect('/api/views/errorLogin')
+  //   }
+  //   const isPasswordValid = await compareData(password,user.password)
+  //   if(!isPasswordValid){
+  //     return res.status(400).json({message:'Email or password not valid'})
+  //   }
+  //   req.session.user = user
+  //   //req.session.email = email
+  //   //req.session.password = password 
+  //   res.redirect('/api/views/profile')
+  // })
+
+
+
+//login con passport
+  userRouter.post(
+  '/login',
+  passport.authenticate('login', {
+    passReqToCallback: true,
+    failureRedirect: '/api/views/errorLogin',
+    successRedirect: '/api/views/profile',
+    failureMessage: '',
+  })
+)
+
+//GitHub
+userRouter.get(
+  '/githubSignup',
+  passport.authenticate('githubSignup', { scope: ['user:email'] })
+)
+
+userRouter.get('/github', 
+  passport.authenticate('githubSignup', { failureRedirect: '/api/views' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/api/views/profile');
+  });
+
+  export default userRouter
