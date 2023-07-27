90fff3d3f0b1a82d01133032098a42e87f860a17
Marianela Grecco <114708413+MarianelaGrecco@users.noreply.github.com>
2023-07-26 23:06:22 -0300

    mocking y manejo de errores

---
+++ Mocking y manejo de errores/src/services/errors/info.js
@@ -0,0 +1,13 @@
+export const ERROR_INFO = {
+    PRODUCT_TITLE_INVALID: "El título del producto debe ser una cadena de texto válida, recibido: {productTitle}",
+    PRODUCT_TYPE_INVALID: "El tipo del producto debe ser una cadena de texto válida, recibido: {productType}",
+    PRODUCT_REQUIRED_INVALID: "El valor de 'required' del producto debe ser un booleano válido, recibido: {productRequired}",
+    PRODUCT_DESCRIPTION_INVALID: "La descripción del producto debe ser una cadena de texto válida, recibido: {productDescription}",
+    PRODUCT_CODE_INVALID: "El código del producto debe ser una cadena de texto válida, recibido: {productCode}",
+    PRODUCT_CATEGORY_INVALID: "La categoría del producto debe ser una cadena de texto válida, recibido: {productCategory}",
+    PRODUCT_PRICE_INVALID: "El precio del producto debe ser un número válido, recibido: {productPrice}",
+    PRODUCT_STOCK_INVALID: "El stock del producto debe ser un número válido, recibido: {productStock}",
+    PRODUCT_STATUS_INVALID: "El estado del producto debe ser uno de los siguientes valores: 'available' o 'out of stock', recibido: {productStatus}",
+  };
+  
+
