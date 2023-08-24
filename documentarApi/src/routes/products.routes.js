20b6fbbea5a2ba5d3fa9c5ceb5830ac670eebc1f
Marianela Grecco <114708413+MarianelaGrecco@users.noreply.github.com>
2023-08-24 17:21:41 -0300

    Documentar Api

---
+++ documentarApi/src/routes/products.routes.js
@@ -0,0 +1,122 @@
+import { Router } from "express";
+import { productsModel } from "../persistencia/mongoDB/models/products.model.js";
+import { isAdmin } from "../authorizationMiddlewere.js";
+import logger from "../utils/logger.js";
+
+const productsRouter = Router();
+
+// Métodos Mongoose
+productsRouter.get("/", async (req, res) => {
+  try {
+    const products = await productsModel.find({}, { __v: 0 });
+    res.status(200).send(products);
+  } catch (error) {
+    logger.error("Error fetching products:", error);
+    res.status(400).send(error);
+  }
+});
+
+productsRouter.post("/nuevoProducto", isAdmin, async (req, res) => {
+  try {
+    const {
+      title,
+      description,
+      price,
+      thumbnail,
+      status,
+      code,
+      category,
+      stock,
+    } = req.body;
+    await productsModel.create({
+      title,
+      description,
+      price,
+      thumbnail,
+      category,
+      status,
+      code,
+      stock,
+    });
+    logger.info("Product created successfully");
+    res.status(200).send("Producto creado correctamente");
+  } catch (error) {
+    logger.error("Error creating product:", error);
+    res.status(400).send(error);
+  }
+});
+
+productsRouter.get("/:pid", async (req, res) => {
+  try {
+    let pid = req.params.pid;
+    const product = await productsModel.findOne({ _id: pid }, { _id: 0, __v: 0 });
+    if (product) {
+      logger.info("Product found:", product);
+      res.status(200).send(product);
+    } else {
+      logger.warn("Product not found with ID:", pid);
+      res.status(200).send("El producto no existe");
+    }
+  } catch (error) {
+    logger.error("Error fetching product:", error);
+    res.status(400).send(error);
+  }
+});
+
+productsRouter.put("/:pid", isAdmin, async (req, res) => {
+  try {
+    let pid = req.params.pid;
+    const modifiedProduct = req.body;
+    await productsModel.updateOne({ _id: pid }, modifiedProduct);
+    logger.info("Product updated successfully");
+    res.status(200).send("El producto se modificó correctamente");
+  } catch (error) {
+    logger.error("Error updating product:", error);
+    res.status(200).send(error);
+  }
+});
+
+productsRouter.delete("/:pid", isAdmin, async (req, res) => {
+  try {
+    const id = req.params.pid;
+    await productsModel.deleteOne({ _id: id });
+    logger.info("Product deleted successfully");
+    res.status(200).send("Producto eliminado correctamente");
+  } catch (error) {
+    logger.error("Error deleting product:", error);
+    res.status(400).send(error);
+  }
+});
+
+// Otras rutas de visualización
+productsRouter.get("/realtimeproducts", async (req, res) => {
+  try {
+    const products = await productsModel.find();
+    res.render("realtimeproducts", { products });
+  } catch (error) {
+    logger.error("Error rendering realtimeproducts view:", error);
+    res.send(error);
+  }
+});
+
+productsRouter.get("/home", async (req, res) => {
+  try {
+    const products = await productsModel.find();
+    res.render("home", { products });
+  } catch (error) {
+    logger.error("Error rendering home view:", error);
+    res.send(error);
+  }
+});
+
+productsRouter.get("/chat", async (req, res) => {
+  try {
+    const products = await productsModel.find();
+    res.render("chat", { products });
+  } catch (error) {
+    logger.error("Error rendering chat view:", error);
+    res.send(error);
+  }
+});
+
+export default productsRouter;
---
+++ documentarApi/src/routes/users.routes.js
@@ -0,0 +1,73 @@
+import { Router } from 'express';
+import { usersModel } from '../persistencia/mongoDB/models/users.model.js';
+import { hashData, compareData } from '../utils/bcrypt.js';
+import passport from 'passport';
+import logger from '../utils/logger.js';
+
+const userRouter = Router();
+
+// Agregar logger a los métodos existentes
+userRouter.get('/', async (req, res) => {
+  try {
+    // ... Código existente para encontrar todos los usuarios ...
+    logger.info('All users fetched successfully:', users);
+    res.status(200).send(users);
+  } catch (error) {
+    logger.error('Error fetching all users:', error);
+    res.status(400).send(error);
+  }
+});
+
+userRouter.get('/:id', async (req, res) => {
+  try {
+    // ... Código existente para encontrar un usuario por ID ...
+    if (user) {
+      logger.info('User found:', user);
+      res.status(200).send(user);
+    } else {
+      logger.warn('User not found with ID:', req.params.id);
+      res.status(404).send('User not found');
+    }
+  } catch (error) {
+    logger.error('Error fetching user:', error);
+    res.status(400).send(error);
+  }
+});
+
+userRouter.post('/', async (req, res) => {
+  try {
+    // ... Código existente para crear un nuevo usuario ...
+    logger.info('New user created successfully:', newUser);
+    res.send(newUser);
+  } catch (error) {
+    logger.error('Error creating user:', error);
+    res.status(400).send(error);
+  }
+});
+
+// persistencia mongo
+userRouter.post('/signup', async (req, res) => {
+  try {
+    // ... Código existente para registrar un nuevo usuario ...
+    logger.info('New user signed up successfully:', newUser);
+    res.redirect('/api/views/signup');
+  } catch (error) {
+    logger.error('Error signing up user:', error);
+    res.redirect('/api/views/errorSignup');
+  }
+});
+
+//   // Login con Passport
+userRouter.post(
+  '/login',
+  passport.authenticate('login', {
+    passReqToCallback: true,
+    failureRedirect: '/api/views/errorLogin',
+    successRedirect: '/api/views/profile',
+    failureMessage: '',
+  })
+);
+
+// ...
+
+export default userRouter;
